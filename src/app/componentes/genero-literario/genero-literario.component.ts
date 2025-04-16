0ceb56c7d7733470bfa04819ef68378ca15884e4
ArthurAizawa <139519439+ArthurAizawa@users.noreply.github.com>
2025-04-16 19:16:26 -0300

    estilização condicional com ngClass

--- src/app/componentes/genero-literario/genero-literario.component.ts
+++ src/app/componentes/genero-literario/genero-literario.component.ts
@@ -1,12 +1,15 @@
 import { Component, input } from '@angular/core';
 import { LivrosComponent } from "../livros/livros.component";
-import { livros } from '../livros/mock-livros';
 import { GeneroLiterario } from '../livros/livro';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-genero-literario',
   standalone: true,
-  imports: [LivrosComponent],
+  imports: [
+    LivrosComponent,
+    CommonModule
+  ],
   templateUrl: './genero-literario.component.html',
   styleUrl: './genero-literario.component.css'
 })
